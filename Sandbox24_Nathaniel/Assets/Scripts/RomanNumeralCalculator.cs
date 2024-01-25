9ae336e787f0b5162791611473358164c7aa259a
Nathaniel Meyer <nmeyer1134@gmail.com>
2024-01-25 14:01:29 -0700

    Made Roman Numeral Translator practice scene

---
+++ Sandbox24_Nathaniel/Assets/Scripts/RomanNumeralCalculator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RomanNumeralCalculator : MonoBehaviour
+{
+    /*  If you don't know how Roman Numerals work, head to this link https://www.dictionary.com/e/roman-numerals/
+     *  
+     *  TODO: Get the input gameobject
+     *  TODO: Make the button grab the input from the user and run the calculation
+     *  TODO: Put the calculation in the output when its done
+     *  
+     *  Bonus Points if you filter the output to only numbers and display an error message if letters are present!
+     *  
+     *  If you think you've done it or would like some guidance, feel free to show me (Nathaniel) in class and I can give you tips
+     */
+
+
+}
