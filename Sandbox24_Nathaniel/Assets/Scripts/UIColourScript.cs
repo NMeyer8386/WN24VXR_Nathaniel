e880c7abcfa7a609e091360cab3106d88db28004
NMeyer8386 <nathaniel.meyer@lethbridgecollege.ca>
2024-02-16 14:37:33 -0700

    finished UI thing and moved some stuff

--- Sandbox24_Nathaniel/Assets/Scripts/UIColourScript.cs
+++ Sandbox24_Nathaniel/Assets/Scripts/UIColourScript.cs
@@ -2,25 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using VInspector;
 
 [ExecuteInEditMode]
 public class UIColourScript : MonoBehaviour
 {
+    //Declare the two colours
     public Color startColour = Color.white;
     public Color endColour = Color.white;
-    [SerializeField] GameObject[] swatchObjects;
+    //Make array for gameobjects to change the colour
+    private Image[] swatchObjects;
+    [SerializeField] GameObject swatchParent;
+    //Declare initial lerp and lerpscale to lerp the colours
     float lerpScale;
     float initLerp;
 
-    [ContextMenu("ChangeColour")]
+    [Button]
     void ChangeColour()
     {
-        initLerp = 1f / swatchObjects.Length;
-        lerpScale = initLerp;
+        swatchObjects = swatchParent.GetComponentsInChildren<Image>();
+
+        //set initial lerp value
+        initLerp = 1f / (swatchObjects.Length - 1);
+        lerpScale = 0;
+
+        //Iterate through each object in the array
         foreach (var obj in swatchObjects)
         {
-            obj.GetComponent<Image>().color = Color.Lerp(startColour, endColour, lerpScale);
+            //Get the colour of the thing and change it based on which object it is
+            obj.color = Color.Lerp(startColour, endColour, lerpScale);
+            //Increment the lerp scale so the gradient changes
             lerpScale += initLerp;
+            
         }
     }
 }
